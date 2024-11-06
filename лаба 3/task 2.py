4b6fd587d3cfa91dee4a5fc5286478f074c0b470
dim7ika <sashademid08@gmail.com>
2024-11-06 22:15:56 +0300

    Update task 2.py

--- лаба 3/task 2.py
+++ лаба 3/task 2.py
@@ -1,10 +1,9 @@
 # TODO Напишите функцию find_common_participants
 def find_common_participants(group1, group2, a=","):
-
- group1_members = group1.split(a)
- group2_members = group2.split(a)
- common_members = sorted(set(group1_members).intersection(set(group2_members)))
- return common_members
+    group1_members = group1.split(a)
+    group2_members = group2.split(a)
+    common_members = set(group1_members).intersection(set(group2_members))
+    return common_members
 
 participants_first_group = "Иванов|Петров|Сидоров"
 participants_second_group = "Петров|Сидоров|Смирнов"
