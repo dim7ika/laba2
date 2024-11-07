453fbc9bc6f11801ef2d5d566983a314101ac77b
dim7ika <sashademid08@gmail.com>
2024-11-08 02:36:54 +0300

    Update task 2.py

--- лаба 3/task 2.py
+++ лаба 3/task 2.py
@@ -2,7 +2,7 @@
 def find_common_participants(group1, group2, a=","):
     group1_members = group1.split(a)
     group2_members = group2.split(a)
-    common_members = set(group1_members).intersection(set(group2_members))
+    common_members = sorted(set(group1_members).intersection(set(group2_members)))
     return common_members
 
 participants_first_group = "Иванов|Петров|Сидоров"
